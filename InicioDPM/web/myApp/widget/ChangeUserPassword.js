6e462c25d1db56bd3525acae44a549645520ee76
evazquez96 <evazquez@logsys.com.mx>
2018-07-18 04:42:00 -0500

    Se modifico el archivo ChangeUserPassword, se agrego la validación de los campos del Formulario

--- InicioDPM/web/myApp/widget/ChangeUserPassword.js
+++ InicioDPM/web/myApp/widget/ChangeUserPassword.js
@@ -9,6 +9,7 @@ define(["dojo/_base/declare",
     "dijit/form/TextBox",
     "dojox/form/PasswordValidator",
     "dijit/form/Button",
+    "dojo/on",
     "dojo/parser"],
 function(declare,
 lang,
@@ -21,6 +22,7 @@ Form,
 TextBox,
 PasswordValidator,
 Button,
+on,
 parser){
     
     return declare([_WidgetBase,_TemplatedMixin,_WidgetsInTemplateMixin],{
@@ -31,7 +33,26 @@ parser){
             var domNode = this.domNode;
             this.inherited(arguments);
             console.log('En postCreate');
-
+            var context=this;
+            on(this.myForm,"submit",function(){
+                if(context.myForm.validate()){
+                    /*
+                     * Dentro de aqui se ara la llamada
+                     * al Servlet para el cambio de 
+                     * Contraseña.
+                     */
+                    return confirm("Adfasdf"); 
+                }
+                else{
+                    alert ("El formulario contiene datos invalidos");
+                    return false;
+                }
+            });
+            on(this.myForm,"reset",function(evt){
+                /*
+                 * Solo resetea los campos
+                 */
+            });
         },
         
         constructor:function(args){
